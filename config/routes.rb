c5a96c76a04ec3fbd31fe7a2ffdd786f99c33f6d
haumer <Alexander.haumer@me.com>
2021-09-18 11:36:44 +0100

    removed a comment

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
   resources :contracts
   resources :reminders, only: [:destroy]
   resources :meetups
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
