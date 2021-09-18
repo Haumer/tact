f97429a7ebf94ac3ca36a6a8dfd6a6ba9f54e76c
haumer <Alexander.haumer@me.com>
2021-09-18 09:29:15 +0100

    added hotwire to meetups in calendar

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -3,6 +3,6 @@ class Meetup < ApplicationRecord
   belongs_to :contact
 
   after_create_commit { broadcast_prepend_to "meetups" }
-  after_update_commit { broadcast_repalce_to "meetups" }
+  after_update_commit { broadcast_replace_to "meetups" }
   after_destroy_commit { broadcast_remove_to "meetups" }
 end
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -4,5 +4,5 @@ class Reminder < ApplicationRecord
 
   after_create_commit { broadcast_prepend_to "reminders" }
   after_destroy_commit { broadcast_remove_to "reminders" }
-  after_update_commit { broadcast_repalce_to "reminders" }
+  after_update_commit { broadcast_replace_to "reminders" }
 end
