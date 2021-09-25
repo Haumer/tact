6b7d88cd9a4d807dd55828b76d80a0cae6458b23
Alex Haumer <alexander.haumer@me.com>
2021-09-25 01:11:14 +0100

    created contacts controller actions

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,41 @@
 class ContactsController < ApplicationController
+  before_action :find_contact, only: [:edit, :update]
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.user = current_user
+    if @contact.save
+      redirect_back(fallback_location: user_contacts_path(user: current_user))
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @contact.update(contact_params)
+      redirect_back(fallback_location: user_contacts_path(user: current_user))
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def find_contact
+    @contact = Contact.find(params[:id])
+  end
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :contact_frequency)
+  end
 end
